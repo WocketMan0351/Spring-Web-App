1b378ce3d3f307482fc3364f413d14de2708a86a
Cody Worthen <cworthen777@hotmail.com>
2022-03-07 19:55:17 -0700

    commit

---
+++ src/main/java/com/worthen/cody/springwebapp/controllers/BookController.java
@@ -0,0 +1,26 @@
+package com.worthen.cody.springwebapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.worthen.cody.springwebapp.repositories.BookRepository;
+
+@Controller // Tells Spring to make this into a Spring MVC Controller
+public class BookController {
+
+	private final BookRepository bookRepository; // injects instance of BookRepository
+
+	public BookController(BookRepository bookRepository) {
+		super();
+		this.bookRepository = bookRepository;
+	}
+
+	@RequestMapping("/books") // maps to a specific URL
+	public String getBooks(Model model) {
+		// Spring provides a model
+		model.addAttribute("books", bookRepository.findAll()); // gets books from the db and assigns it to "books"
+		return "books"; // returns a string that tells Spring MVC to apply the view "books"
+	}
+
+}
