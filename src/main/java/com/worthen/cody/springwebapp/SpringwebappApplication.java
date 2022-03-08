c23323970d54858d6ed6eda0930f90a5488bf576
Cody Worthen <cworthen777@hotmail.com>
2022-03-07 17:08:09 -0700

    database can now be viewed through the web GUI

--- src/main/java/com/worthen/cody/springwebapp/SpringwebappApplication.java
+++ src/main/java/com/worthen/cody/springwebapp/SpringwebappApplication.java
@@ -1,25 +1,15 @@
 package com.worthen.cody.springwebapp;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// This app uses Hibernate and Spring Data JPA to avoid having to write SQL
 
 @SpringBootApplication
 public class SpringwebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringwebappApplication.class, args);
-		Set<String> numbers = new HashSet<>();
-		numbers.add("14");
-		numbers.add("3");
-		numbers.add("1");
-		System.out.println(numbers.add("1"));
-
-		for (String s : numbers) {
-			System.out.println(s);
-		}
 	}
 
 }
